b596a628f0eb3bb51fccf178488db9379f909dcc
atzisb-kozak <306156@supinfo.com>
2020-03-22 11:42:00 +0400

    Flip qui remarchesamère

--- js/jeumemo.js
+++ js/jeumemo.js
@@ -3,8 +3,8 @@ let lastcard = "";
 let backcard = "img/card/CARDS4.png";
 let tablemodel=["JDG","IRONMAN","ARIANA","JB","MINECRAFT","WORLD"];
 let tablecard=[];
-let collums=6;
-let row=3;
+let collums=0;
+let row=0;
 class card{
     constructor(imgback,imgfront,idcard)
     {
@@ -64,13 +64,12 @@ class card{
 }
 $(document).ready(()=>{
     setGame();
-    
-    game();
 });
 function setGame()
 {
-    $('.container').click(function(){
+    $('.bc').click(function(){
         var backimg = $(this).children().attr("src");
+        backcard = backimg;
         backimg=backimg.substring(9,backimg.length-4);
         $('#selectbc').text("Vous avez choisi le fond "+backimg)
     });
@@ -80,34 +79,59 @@ function setGame()
         {
             row=2;
             collums=3;
-            $('.table').css("grid-template-columns","repeat("+collums+",1fr)");
+            //$('.table').css("grid-template-columns","repeat("+collums+",1fr)");
             $('#selectlv').text("Vous avez choisi le mode "+row+"/"+collums+"; Appuyer sur JOUER pour commencer");
         }
         if($(this).attr("id")=="3/6")
         {
             row=3;
             collums=6;
-            $('.table').css("grid-template-columns","repeat("+collums+",1fr)");
+            //$('.table').css("grid-template-columns","repeat("+collums+",1fr)");
             $('#selectlv').text("Vous avez choisi le mode "+row+"/"+collums+"; Appuyer sur JOUER pour commencer");
         }
         if($(this).attr("id")=="3/12")
         {
             row=3;
             collums=12;
-            $('.table').css("grid-template-columns","repeat("+collums+",1fr)");
+            //$('.table').css("grid-template-columns","repeat("+collums+",1fr)");
             $('#selectlv').text("Vous avez choisi le mode "+row+"/"+collums+"; Appuyer sur JOUER pour commencer");
         }
         if($(this).attr("id")=="JOUER")
         {
-            $('.edit_niveau').addClass("displaynone");
-            $('.gameinfo').removeClass("displaynone");
-            setTable();
-            console.log(tablecard);
-            shuffle(tablecard);
-            for (var i=0;i<tablecard.length;i++)
+            if(collums == 0 && row==0)
             {
-                tablecard[i].set_tags();
+                $('#selectlv').text("Vous devez choisir un mode pour jouer");
+            }else{
+                $('.edit_niveau').addClass("displaynone");
+                $('.gameinfo').removeClass("displaynone");
+                setTable();
+                console.log(tablecard);
+                shuffle(tablecard);
+                var i =0;
+                while(i<tablecard.length)
+                {
+                    tablecard[i].set_tags();
+                    i++;
+                }
             }
+            game();
+        }
+    });
+}
+function game()
+{
+    $('.card').click(function(){
+        var testid=$(this).attr("id");
+        for (var i = 0;i<tablecard.length;i++)
+        {
+            if (tablecard[i].id==testid)
+            {
+                var j=i;
+            }
+        }
+        if (!(tablecard[j].isFlipped))
+        {
+            tablecard[j].flip();
         }
     });
 }
@@ -125,25 +149,6 @@ function setTable()
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
-function game()
-{
-    $('.card').click(function(){
-        var testid=$(this).attr("id");
-        for (var i = 0;i<tablecard.length;i++)
-        {
-            if (tablecard[i].id==testid)
-            {
-                var j=i;
-            }
-        }
-        if (!(tablecard[j].isFlipped))
-        {
-            tablecard[j].flip();
-            peerCondition()
-        }
-    });
-}
-
 function peerCondition(id,classes)
 {
     let id2='#'+id;
